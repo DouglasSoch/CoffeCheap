d749b382e89e6ba6f18157dc61209212a8f96ebf
medev <medev@instructor10-OptiPlex-7010>
2017-08-30 11:46:53 -0600

    Merge origin/master
    Conflicts:
    	NetBeansProjects/operaciones/src/operaciones/Operaciones.java

---
+++ CoffeCheap/src/java/com/coffecheap/bean/MesaBean.java
@@ -0,0 +1,94 @@
+package com.coffecheap.bean;
+
+/**
+ *
+ * @marhor acier
+ */
+
+import com.coffecheap.modelo.Mesa;
+import com.coffecheap.dao.MesaDao;
+
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+//import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class MesaBean {
+
+  private Mesa mesa = new Mesa();
+  private List<Mesa> lstMesa;
+
+  public Mesa getMesa() {
+    return mesa;
+  }
+
+  public void setMesa(Mesa mesa) {
+    this.mesa = mesa;
+  }
+
+  public List<Mesa> getLstMesa() {
+    return lstMesa;
+  }
+
+  public void setLstMesa(List<Mesa> lstMesa) {
+    this.lstMesa = lstMesa;
+  }
+
+  public void registrar() {
+
+    System.out.println("*******************************************************registrar");
+
+    MesaDao dao;
+
+    try {
+      dao = new MesaDao();
+      dao.registrar(mesa);
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+
+  }
+
+  public void listar() throws Exception {
+    MesaDao dao;
+
+    try {
+      dao = new MesaDao();
+      lstMesa = dao.listar();
+    } catch (Exception e) {
+      throw e;
+    }
+
+  }
+
+  public void modificar(Mesa mar) throws Exception {
+    System.out.println("*******************************************************Modificar");
+    MesaDao dao;
+
+    try {
+      dao = new MesaDao();
+      dao.modificar(mar);
+
+    } catch (Exception e) {
+      throw e;
+    }
+
+  }
+
+  public void eliminar(Mesa mar) throws Exception {
+    System.out.println("*******************************************************eliminar");
+    MesaDao dao;
+
+    try {
+      dao = new MesaDao();
+      dao.eliminar(mar);
+
+    } catch (Exception e) {
+      throw e;
+    }
+
+  }
+
+}
---
+++ CoffeCheap/src/java/com/coffecheap/dao/MesaDao.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.coffecheap.dao;
+
+import com.coffecheap.modelo.Mesa;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author acier
+ */
+public class MesaDao extends Dao {
+
+  public void registrar(Mesa Tt) throws Exception {
+
+    try {
+      this.Conectar();
+      PreparedStatement st = this.getCon().prepareStatement("insert into mesa values(?,?,?,?,?);");
+
+      st.setInt(1, Tt.getId_mesa());
+      st.setInt(2, Tt.getCantidad_comensales());
+      st.setString(3, Tt.getUbicacion());
+     st.setInt(4, Tt.getReserva().getId_reserva());
+     st.setInt(5, Tt.getCliente().getId_cliente());
+
+      st.executeUpdate();
+
+    } catch (Exception ex) {
+      throw ex;
+    } finally {
+      this.Desconecar();
+
+    }
+
+  }
+
+  public List<Mesa> listar() throws Exception {
+    List<Mesa> lista;
+    ResultSet rs;
+
+    try {
+      this.Conectar();
+      PreparedStatement st = this.getCon().prepareCall("SELECT *FROM mesa");
+      rs = st.executeQuery();
+      lista = new ArrayList();
+      while (rs.next()) {
+        Mesa tt = new Mesa();
+
+        tt.setId_mesa(rs.getInt(1));
+        tt.setCantidad_comensales(rs.getInt(2));
+        tt.setUbicacion(rs.getString(3));
+        tt.getReserva().setId_reserva(rs.getInt(4));
+        tt.getCliente().setId_cliente(rs.getInt(5));
+        lista.add(tt);
+      }
+
+    } catch (Exception ex) {
+      throw ex;
+    } finally {
+      this.Desconecar();
+    }
+
+    return lista;
+
+  }
+
+  public void modificar(Mesa tt) throws Exception {
+    System.out.println("*******************************************************modificar dao");
+    try {
+      this.Conectar();
+      PreparedStatement st = this.getCon().prepareStatement("UPDATE  mesa SET id_Mesa=?, cantidad_Comensales=?, Ubicacion=?, id_reserva=?, id_cliente=? WHERE id_Mesa=?;");
+
+      st.setInt(1, tt.getId_mesa());
+      st.setInt(2, tt.getCantidad_comensales());
+      st.setString(3, tt.getUbicacion());
+      st.setInt(4, tt.getReserva().getId_reserva());
+      st.setInt(5, tt.getCliente().getId_cliente());
+      
+      st.setInt(6, tt.getId_mesa());
+
+      st.executeUpdate();
+
+    } catch (Exception ex) {
+      throw ex;
+    } finally {
+      this.Desconecar();
+
+    }
+
+  }
+
+  public void eliminar(Mesa pac) throws Exception {
+    System.out.println("*******************************************************eliminar dao");
+    try {
+      this.Conectar();
+      PreparedStatement st = this.getCon().prepareStatement("DELETE FROM mesa WHERE id_Mesa=?;");
+      st.setInt(1, pac.getId_mesa());
+      st.executeUpdate();
+
+    } catch (Exception ex) {
+      throw ex;
+    } finally {
+      this.Desconecar();
+
+    }
+
+  }
+
+}
