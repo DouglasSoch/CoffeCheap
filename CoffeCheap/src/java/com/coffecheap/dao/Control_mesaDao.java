a075cc79a5388851203b38dbe3837c8cfe57fcfe
medev <medev@instructor10-OptiPlex-7010>
2017-09-27 13:00:53 -0600

    Merge origin/master
    Conflicts:
    	CoffeCheap/nbproject/project.properties
    	CoffeCheap/src/java/com/coffecheap/dao/Pedido_MostrarDao.java

--- CoffeCheap/src/java/com/coffecheap/dao/Control_mesaDao.java
+++ CoffeCheap/src/java/com/coffecheap/dao/Control_mesaDao.java
@@ -18,88 +18,107 @@ import java.util.List;
  */
 public class Control_mesaDao extends Dao {
 
-  public void registrar(Control_mesa Tt) throws Exception {
+    public void registrar(Control_mesa Tt) throws Exception {
 
-    try {
-      this.Conectar();
-      PreparedStatement st = this.getCon().prepareStatement("insert into control_mesa values(?,?);");
+        try {
+            this.Conectar();
+            PreparedStatement st = this.getCon().prepareStatement("insert into control_mesa values(?,?);");
 
-      //st.setInt(1, Tt.getId_tipo());
-      //st.setString(2, Tt.getNombre());
-      st.executeUpdate();
+            //st.setInt(1, Tt.getId_tipo());
+            //st.setString(2, Tt.getNombre());
+            st.executeUpdate();
 
-    } catch (Exception ex) {
-      throw ex;
-    } finally {
-      this.Desconecar();
+        } catch (Exception ex) {
+            throw ex;
+        } finally {
+            this.Desconecar();
 
-    }
-
-  }
-
-  public List<Control_mesa> listarMesaOcupadas() throws Exception {
-    List<Control_mesa> lista;
-    ResultSet rs;
-
-    try {
-      this.Conectar();
-      PreparedStatement st = this.getCon().prepareCall("SELECT *FROM control_mesa");
-      rs = st.executeQuery();
-      lista = new ArrayList();
-      while (rs.next()) {
-        Control_mesa tt = new Control_mesa();
-
-        //  tt.setId_tipo(rs.getInt(1));
-        //  tt.setNombre(rs.getString(2));
-        lista.add(tt);
-      }
-
-    } catch (Exception ex) {
-      throw ex;
-    } finally {
-      this.Desconecar();
-    }
-
-    return lista;
-
-  }
-
-  public void eliminar(Control_mesa pac) throws Exception {
-    System.out.println("*******************************************************eliminar dao");
-    try {
-      this.Conectar();
-      PreparedStatement st = this.getCon().prepareStatement("DELETE FROM control_mesa WHERE id_control_mesa=?;");
-      //st.setInt(1, pac.getId_tipo());
-      st.executeUpdate();
-
-    } catch (Exception ex) {
-      throw ex;
-    } finally {
-      this.Desconecar();
+        }
 
     }
 
-  }
+    public List<Control_mesa> listarMesaOcupadas() throws Exception {
+        List<Control_mesa> lista;
+        ResultSet rs;
 
-  public void CamBioEstado(int bus) throws Exception {
+        try {
+            this.Conectar();
+            PreparedStatement st = this.getCon().prepareCall("SELECT *FROM control_mesa");
+            rs = st.executeQuery();
+            lista = new ArrayList();
+            while (rs.next()) {
+                Control_mesa tt = new Control_mesa();
 
-    System.out.println("*******************************************************modificar dao");
-    try {
-      this.Conectar();
-      PreparedStatement st = this.getCon().prepareStatement("UPDATE  mesa SET id_estado=? WHERE id_mesa=?;");
+                //  tt.setId_tipo(rs.getInt(1));
+                //  tt.setNombre(rs.getString(2));
+                lista.add(tt);
+            }
 
-      st.setInt(1, 3);
-      st.setInt(1, bus);
+        } catch (Exception ex) {
+            throw ex;
+        } finally {
+            this.Desconecar();
+        }
 
-      st.executeUpdate();
-
-    } catch (Exception ex) {
-      throw ex;
-    } finally {
-      this.Desconecar();
+        return lista;
 
     }
 
-  }
+    public int tamaño() throws Exception {
+        int numero=0;
+        try {
+            this.Conectar();
+            PreparedStatement st = this.getCon().prepareCall("select count(*)from mesa");
+            ResultSet n = st.executeQuery();
+            
+            if (n.next()) {
+            numero = n.getInt(1);
+            }
+        } catch (Exception ex) {
+            throw ex;
+        } finally {
+            this.Desconecar();
+        }
+
+        return numero;
+    }
+
+    public void eliminar(Control_mesa pac) throws Exception {
+        System.out.println("*******************************************************eliminar dao");
+        try {
+            this.Conectar();
+            PreparedStatement st = this.getCon().prepareStatement("DELETE FROM control_mesa WHERE id_control_mesa=?;");
+            //st.setInt(1, pac.getId_tipo());
+            st.executeUpdate();
+
+        } catch (Exception ex) {
+            throw ex;
+        } finally {
+            this.Desconecar();
+
+        }
+
+    }
+
+    public void CamBioEstado(int bus) throws Exception {
+
+        System.out.println("*******************************************************modificar dao");
+        try {
+            this.Conectar();
+            PreparedStatement st = this.getCon().prepareStatement("UPDATE  mesa SET id_estado=? WHERE id_mesa=?;");
+
+            st.setInt(1, 3);
+            st.setInt(1, bus);
+
+            st.executeUpdate();
+
+        } catch (Exception ex) {
+            throw ex;
+        } finally {
+            this.Desconecar();
+
+        }
+
+    }
 
 }
