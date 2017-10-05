4e3cb468ad900537466599837635bc76cf6ce049
medev <medev@instructor10-OptiPlex-7010>
2017-10-05 12:30:52 -0600

    gays

--- CoffeCheap/src/java/com/coffecheap/bean/Control_mesaBean.java
+++ CoffeCheap/src/java/com/coffecheap/bean/Control_mesaBean.java
@@ -7,20 +7,17 @@ package com.coffecheap.bean;
 import com.coffecheap.modelo.Control_mesa;
 import com.coffecheap.modelo.EstadoPago;
 import com.coffecheap.dao.Control_mesaDao;
-import com.coffecheap.dao.MesaDao;
-
 import java.util.List;
 import javax.faces.bean.ManagedBean;
-//import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
 
 @ManagedBean
 @ViewScoped
 public class Control_mesaBean {
 
-  private Control_mesa control_mesa = new Control_mesa();
-  private List<Control_mesa> lstControl_mesa;
-  private EstadoPago datoEstado;
+    private Control_mesa control_mesa = new Control_mesa();
+    private List<Control_mesa> lstControl_mesa;
+    private EstadoPago datoEstado;
 
     public EstadoPago getDatoEstado() {
         return datoEstado;
@@ -30,147 +27,159 @@ public class Control_mesaBean {
         this.datoEstado = datoEstado;
     }
 
+    public Control_mesa getControl_mesa() {
+        return control_mesa;
+    }
+
+    public void setControl_mesa(Control_mesa control_mesa) {
+        this.control_mesa = control_mesa;
+    }
+
+    public List<Control_mesa> getLstControl_mesa() {
+        return lstControl_mesa;
+    }
+
+    public void setLstControl_mesa(List<Control_mesa> lstControl_mesa) {
+        this.lstControl_mesa = lstControl_mesa;
+    }
+
+    public void registrar() {
+
+        System.out.println("*******************************************************registrar");
+
+        Control_mesaDao dao;
+
+        try {
+            dao = new Control_mesaDao();
+            dao.registrar(control_mesa);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+    public void listar() throws Exception {
+        Control_mesaDao dao;
+
+        try {
+            dao = new Control_mesaDao();
+            lstControl_mesa = dao.listarMesaOcupadas();
+        } catch (Exception e) {
+            throw e;
+        }
+
+    }
+
+    public int tamano() throws Exception {
+        Control_mesaDao dao;
+        int numero = 0;
+        try {
+            dao = new Control_mesaDao();
+            numero = dao.tamaño();
+        } catch (Exception e) {
+            throw e;
+        }
+        System.out.println("El numero de mesas es " + numero);
+        return numero;
+
+    }
+
+    public void eliminar(Control_mesa mar) throws Exception {
+        System.out.println("*******************************************************eliminar");
+        Control_mesaDao dao;
+
+        try {
+            dao = new Control_mesaDao();
+            dao.eliminar(mar);
+
+        } catch (Exception e) {
+            throw e;
+        }
+
+    }
+
+    public void CambioEstado(int MesaSelecionada) throws Exception {
+        System.out.println("********************************Cambio Estado de mesa");
+        Control_mesaDao dao;
+
+        try {
+            dao = new Control_mesaDao();
+            dao.CamBioEstado(MesaSelecionada);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public boolean HabilitarPago(int mesa) throws Exception {
+        System.out.println("********************************Pago de mesa");
+        Control_mesaDao dao;
+        boolean pago = false;
+        try {
+            dao = new Control_mesaDao();
+            pago = dao.HabilitarPago(mesa);
+        } catch (Exception e) {
+            throw e;
+        }
+
+        System.out.println("" + mesa + "* " + pago + "");
+        return pago;
+    }
+
+    public String EstaPago(int mesa) throws Exception {
+        System.out.println("********************************Pago de mesa");
+        Control_mesaDao dao;
+        String pago = null;
+        try {
+            dao = new Control_mesaDao();
+            pago = dao.EstaPago(mesa);
+        } catch (Exception e) {
+            throw e;
+        }
+
+        System.out.println("" + mesa + "* " + pago + "");
+        return pago;
+    }
+
+    public String ControlEstado(int mesa) throws Exception {
+        System.out.println("********************************Pago de mesa");
+        Control_mesaDao dao;
+        String estado = null;
+        try {
+            dao = new Control_mesaDao();
+            estado = dao.ControlEstado(mesa);
+        } catch (Exception e) {
+            throw e;
+        }
+
+        System.out.println("" + mesa + "* " + estado + "");
+        return estado;
+    }
+
+    public void RegistrarPago(int Mesa) throws Exception {
+        System.out.println("********************************Registrar Pago");
+        Control_mesaDao dao;
+
+        try {
+            dao = new Control_mesaDao();
+            dao.RegistrarPago(Mesa);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
     
+    public void LimpiarMesa(int Mesa) throws Exception {
+        System.out.println("********************************Limpiar Mesa");
+        Control_mesaDao dao;
 
-  public Control_mesa getControl_mesa() {
-    return control_mesa;
-  }
-
-  public void setControl_mesa(Control_mesa control_mesa) {
-    this.control_mesa = control_mesa;
-  }
-
-  public List<Control_mesa> getLstControl_mesa() {
-    return lstControl_mesa;
-  }
-
-  public void setLstControl_mesa(List<Control_mesa> lstControl_mesa) {
-    this.lstControl_mesa = lstControl_mesa;
-  }
-
-  public void registrar() {
-
-    System.out.println("*******************************************************registrar");
-
-    Control_mesaDao dao;
-
-    try {
-      dao = new Control_mesaDao();
-      dao.registrar(control_mesa);
-    } catch (Exception e) {
-      System.out.println(e);
+        try {
+            dao = new Control_mesaDao();
+            dao.LimpiarMesa(Mesa);
+        } catch (Exception e) {
+            throw e;
+        }
     }
 
-  }
-
-  public void listar() throws Exception {
-    Control_mesaDao dao;
-
-    try {
-      dao = new Control_mesaDao();
-      lstControl_mesa = dao.listarMesaOcupadas();
-    } catch (Exception e) {
-      throw e;
-    }
-
-  }
-
-  public int tamano() throws Exception {
-    Control_mesaDao dao;
-    int numero = 0;
-    try {
-      dao = new Control_mesaDao();
-      numero = dao.tamaño();
-    } catch (Exception e) {
-      throw e;
-    }
-    System.out.println("El numero de mesas es "+numero);
-    return numero;
     
-  }
-
-  public void eliminar(Control_mesa mar) throws Exception {
-    System.out.println("*******************************************************eliminar");
-    Control_mesaDao dao;
-
-    try {
-      dao = new Control_mesaDao();
-      dao.eliminar(mar);
-
-    } catch (Exception e) {
-      throw e;
-    }
-
-  }
-
-  public void CambioEstado(int MesaSelecionada) throws Exception {
-    System.out.println("********************************Cambio Estado de mesa");
-    Control_mesaDao dao;
-
-    try {
-      dao = new Control_mesaDao();
-      dao.CamBioEstado(MesaSelecionada);
-    } catch (Exception e) {
-      throw e;
-    }
-  }
-  
- public boolean HabilitarPago(int mesa) throws Exception{
-    System.out.println("********************************Pago de mesa");
-    Control_mesaDao dao;
-    boolean pago=false;
-    try {
-      dao = new Control_mesaDao();
-      pago=dao.HabilitarPago(mesa);
-    } catch (Exception e) {
-      throw e;
-    }
-    
-    System.out.println(""+mesa+"* "+pago+"");
-    return pago;
-  }
- 
-  public String EstaPago(int mesa) throws Exception{
-    System.out.println("********************************Pago de mesa");
-    Control_mesaDao dao;
-    String pago=null;
-    try {
-      dao = new Control_mesaDao();
-      pago=dao.EstaPago(mesa);
-    } catch (Exception e) {
-      throw e;
-    }
-    
-    System.out.println(""+mesa+"* "+pago+"");
-    return pago;
-  }
-  
-  public String ControlEstado(int mesa) throws Exception{
-    System.out.println("********************************Pago de mesa");
-    Control_mesaDao dao;
-    String estado=null;
-    try {
-      dao = new Control_mesaDao();
-      estado=dao.ControlEstado(mesa);
-    } catch (Exception e) {
-      throw e;
-    }
-    
-    System.out.println(""+mesa+"* "+estado+"");
-    return estado;
-  }
- 
-  public void RegistrarPago(int Mesa) throws Exception {
-    System.out.println("********************************Registrar Pago");
-    Control_mesaDao dao;
-
-    try {
-      dao = new Control_mesaDao();
-      dao.RegistrarPago(Mesa);
-    } catch (Exception e) {
-      throw e;
-    }
-  }
 // public EstadoPago DatosEstados(int mesa) throws Exception {
 //     
 //     Control_mesaDao dao;
@@ -186,8 +195,5 @@ public class Control_mesaBean {
 //    return datoEstado;
 //
 //  }
- 
- 
-
 
 }
--- CoffeCheap/src/java/com/coffecheap/dao/Control_mesaDao.java
+++ CoffeCheap/src/java/com/coffecheap/dao/Control_mesaDao.java
@@ -19,278 +19,274 @@ import java.util.List;
  */
 public class Control_mesaDao extends Dao {
 
-    public void registrar(Control_mesa Tt) throws Exception {
+  public void registrar(Control_mesa Tt) throws Exception {
 
-        try {
-            this.Conectar();
-            PreparedStatement st = this.getCon().prepareStatement("insert into control_mesa values(?,?);");
+    try {
+      this.Conectar();
+      PreparedStatement st = this.getCon().prepareStatement("insert into control_mesa values(?,?);");
 
-            //st.setInt(1, Tt.getId_tipo());
-            //st.setString(2, Tt.getNombre());
-            st.executeUpdate();
+      //st.setInt(1, Tt.getId_tipo());
+      //st.setString(2, Tt.getNombre());
+      st.executeUpdate();
 
-        } catch (Exception ex) {
-            throw ex;
-        } finally {
-            this.Desconecar();
-
-        }
+    } catch (Exception ex) {
+      throw ex;
+    } finally {
+      this.Desconecar();
 
     }
 
-    public List<Control_mesa> listarMesaOcupadas() throws Exception {
-        List<Control_mesa> lista;
-        ResultSet rs;
+  }
 
-        try {
-            this.Conectar();
-            PreparedStatement st = this.getCon().prepareCall("SELECT *FROM control_mesa");
-            rs = st.executeQuery();
-            lista = new ArrayList();
-            while (rs.next()) {
-                Control_mesa tt = new Control_mesa();
+  public List<Control_mesa> listarMesaOcupadas() throws Exception {
+    List<Control_mesa> lista;
+    ResultSet rs;
 
-                //  tt.setId_tipo(rs.getInt(1));
-                //  tt.setNombre(rs.getString(2));
-                lista.add(tt);
-            }
+    try {
+      this.Conectar();
+      PreparedStatement st = this.getCon().prepareCall("SELECT *FROM control_mesa");
+      rs = st.executeQuery();
+      lista = new ArrayList();
+      while (rs.next()) {
+        Control_mesa tt = new Control_mesa();
 
-        } catch (Exception ex) {
-            throw ex;
-        } finally {
-            this.Desconecar();
-        }
+        //  tt.setId_tipo(rs.getInt(1));
+        //  tt.setNombre(rs.getString(2));
+        lista.add(tt);
+      }
 
-        return lista;
+    } catch (Exception ex) {
+      throw ex;
+    } finally {
+      this.Desconecar();
+    }
+
+    return lista;
+
+  }
+
+  public int tamaño() throws Exception {
+    int numero = 0;
+    try {
+      this.Conectar();
+      PreparedStatement st = this.getCon().prepareCall("select count(*)from mesa");
+      ResultSet n = st.executeQuery();
+
+      if (n.next()) {
+        numero = n.getInt(1);
+      }
+    } catch (Exception ex) {
+      throw ex;
+    } finally {
+      this.Desconecar();
+    }
+
+    return numero;
+  }
+
+  public void eliminar(Control_mesa pac) throws Exception {
+    System.out.println("*******************************************************eliminar dao");
+    try {
+      this.Conectar();
+      PreparedStatement st = this.getCon().prepareStatement("DELETE FROM control_mesa WHERE id_control_mesa=?;");
+      //st.setInt(1, pac.getId_tipo());
+      st.executeUpdate();
+
+    } catch (Exception ex) {
+      throw ex;
+    } finally {
+      this.Desconecar();
 
     }
 
-    public int tamaño() throws Exception {
-        int numero = 0;
-        try {
-            this.Conectar();
-            PreparedStatement st = this.getCon().prepareCall("select count(*)from mesa");
-            ResultSet n = st.executeQuery();
+  }
 
-            if (n.next()) {
-                numero = n.getInt(1);
-            }
-        } catch (Exception ex) {
-            throw ex;
-        } finally {
-            this.Desconecar();
-        }
+  public void CamBioEstado(int mesa) throws Exception {
 
-        return numero;
-    }
+    System.out.println("*******************************************************modificar dao");
+    try {
+      this.Conectar();
+      PreparedStatement st = this.getCon().prepareStatement("UPDATE  mesa SET id_estado=? WHERE id_mesa=?;");
+      st.setInt(1, 3);
+      st.setInt(2, mesa);
+      st.executeUpdate();
 
-    public void eliminar(Control_mesa pac) throws Exception {
-        System.out.println("*******************************************************eliminar dao");
-        try {
-            this.Conectar();
-            PreparedStatement st = this.getCon().prepareStatement("DELETE FROM control_mesa WHERE id_control_mesa=?;");
-            //st.setInt(1, pac.getId_tipo());
-            st.executeUpdate();
-
-        } catch (Exception ex) {
-            throw ex;
-        } finally {
-            this.Desconecar();
-
-        }
+    } catch (Exception ex) {
+      throw ex;
+    } finally {
+      this.Desconecar();
 
     }
 
-    public void CamBioEstado(int mesa) throws Exception {
+  }
 
-        System.out.println("*******************************************************modificar dao");
-        try {
-            this.Conectar();
-            PreparedStatement st = this.getCon().prepareStatement("UPDATE  mesa SET id_estado=? WHERE id_mesa=?;");
-            st.setInt(1, 3);
-            st.setInt(2, mesa);
-            st.executeUpdate();
+  public String ControlEstado(int mesa) throws Exception {
+    String color = "rgb(115, 191, 209)";
+    System.out.println("*******************************************************modificar dao");
+    try {
+      this.Conectar();
+      PreparedStatement s2 = this.getCon().prepareStatement("select id_estado from mesa WHERE id_mesa=?;");
+      s2.setInt(1, mesa);
+      ResultSet n = s2.executeQuery();
 
-        } catch (Exception ex) {
-            throw ex;
-        } finally {
-            this.Desconecar();
+      if (n.next()) {
+        if (n.getInt(1) == 1) {
+          color = "rgb(115, 191, 209)";
+        } else {
+          color = "rgb(150, 60, 60)";
+        }
+
+      }
+
+    } catch (Exception ex) {
+      throw ex;
+    } finally {
+      this.Desconecar();
+    }
+
+    return color;
+
+  }
+
+  public void RegistrarPago(int mesa) throws Exception {
+
+    System.out.println("*******************************************************modificar dao");
+    try {
+      this.Conectar();
+      int idPedido = 0;
+      PreparedStatement s2 = this.getCon().prepareStatement("select id_pedido from control where id_mesa=?;");
+      s2.setInt(1, mesa);
+      ResultSet n = s2.executeQuery();
+
+      if (n.next()) {
+        idPedido = n.getInt(1);
+
+        System.out.println("El pedido = " + idPedido);
+      }
+
+      PreparedStatement esme = this.getCon().prepareStatement("select  id_estado from mesa WHERE id_mesa=?;");
+      esme.setInt(1, mesa);
+      ResultSet n_est = esme.executeQuery();
+
+      if (n_est.next()) {
+
+        if (n_est.getInt(1) > 1) {
+          PreparedStatement st = this.getCon().prepareStatement("UPDATE pedido SET cancelado=1 WHERE id_pedido =?;");
+         st.setInt(1, idPedido);
+          st.executeUpdate();
 
         }
+      }
+
+    } catch (Exception ex) {
+      throw ex;
+    } finally {
+      this.Desconecar();
 
     }
 
-    public String ControlEstado(int mesa) throws Exception {
-        String color = "rgb(115, 191, 209)";
-        System.out.println("*******************************************************modificar dao");
-        try {
-            this.Conectar();
-            PreparedStatement s2 = this.getCon().prepareStatement("select id_estado from mesa WHERE id_mesa=?;");
-            s2.setInt(1, mesa);
-            ResultSet n = s2.executeQuery();
+  }
 
-            if (n.next()) {
-                if (n.getInt(1) == 1) {
-                    color = "rgb(115, 191, 209)";
-                } else {
-                    color = "rgb(150, 60, 60)";
-                }
+  public boolean HabilitarPago(int mesa) throws Exception {
+    int pago;
+    boolean estado = false;
+    try {
+      this.Conectar();
 
-            }
+      PreparedStatement s2 = this.getCon().prepareStatement("select cancelado from control"
+              + " where id_pedido in"
+              + " (select max(id_pedido) from control group by id_mesa) and id_mesa=?;");
+      s2.setInt(1, mesa);
+      ResultSet n = s2.executeQuery();
 
-        } catch (Exception ex) {
-            throw ex;
-        } finally {
-            this.Desconecar();
+      if (n.next()) {
+        pago = n.getInt(1);
+        System.out.println("DAO: PAGO: " + pago);
+        if (pago == 1) {
+          estado = true;
+        } else {
+          estado = false;
         }
+      }
 
-        return color;
+    } catch (Exception ex) {
+      throw ex;
+    } finally {
+      this.Desconecar();
 
     }
+    System.out.println("DAO ESTADO: " + estado);
+    return estado;
+  }
 
-    public void RegistrarPago(int mesa) throws Exception {
+  public String EstaPago(int mesa) throws Exception {
+    int pago;
+    String estado = null;
+    try {
+      this.Conectar();
 
-        System.out.println("*******************************************************modificar dao");
-        try {
-            this.Conectar();
-            int idPedido = 0;
-            PreparedStatement s2 = this.getCon().prepareStatement("select id_pedido from control where id_mesa=?;");
-            s2.setInt(1, mesa);
-            ResultSet n = s2.executeQuery();
+      PreparedStatement s2 = this.getCon().prepareStatement("select cancelado from control"
+              + " where id_pedido in"
+              + " (select max(id_pedido) from control group by id_mesa) and id_mesa=?;");
+      s2.setInt(1, mesa);
+      ResultSet n = s2.executeQuery();
 
-            if (n.next()) {
-                idPedido = n.getInt(1);
-
-                System.out.println("El pedido = " + idPedido);
-            }
-
-            PreparedStatement esme = this.getCon().prepareStatement("select  id_estado from mesa WHERE id_mesa=?;");
-            s2.setInt(1, mesa);
-            ResultSet n_est = esme.executeQuery();
-
-            if (n_est.next()) {
-                System.out.println("if fuera");
-                if (n_est.getInt(1)>1) {
-                    PreparedStatement st = this.getCon().prepareStatement("UPDATE pedido SET cancelado=1 WHERE id_pedido =" + idPedido + ";");
-                    st.executeUpdate();
-                    System.out.println("if dentro");
-                }
-            } else {
-                System.out.println("no entro bien");
-            }
-
-        } catch (Exception ex) {
-            throw ex;
-        } finally {
-            this.Desconecar();
-
+      if (n.next()) {
+        pago = n.getInt(1);
+        System.out.println("DAO: PAGO: " + pago);
+        if (pago == 1) {
+          estado = "Cancelado";
+        } else {
+          estado = "Pagado";
         }
+      }
+
+    } catch (Exception ex) {
+      throw ex;
+    } finally {
+      this.Desconecar();
 
     }
+    System.out.println("DAO ESTADO: " + estado);
+    return estado;
+  }
 
-    public boolean HabilitarPago(int mesa) throws Exception {
-        int pago;
-        boolean estado = false;
-        try {
-            this.Conectar();
+  public void LimpiarMesa(int mesa) throws Exception {
 
-            PreparedStatement s2 = this.getCon().prepareStatement("select cancelado from control"
-                    + " where id_pedido in"
-                    + " (select max(id_pedido) from control group by id_mesa) and id_mesa=?;");
-            s2.setInt(1, mesa);
-            ResultSet n = s2.executeQuery();
+    System.out.println("*******************************************************Limpiar dao");
+    try {
+      this.Conectar();
 
-            if (n.next()) {
-                pago = n.getInt(1);
-                System.out.println("DAO: PAGO: " + pago);
-                if (pago == 1) {
-                    estado = true;
-                } else {
-                    estado = false;
-                }
-            }
+      PreparedStatement esme = this.getCon().prepareStatement("select  id_estado from mesa WHERE id_mesa=?;");
+      esme.setInt(1, mesa);
+      ResultSet n_est = esme.executeQuery();
 
-        } catch (Exception ex) {
-            throw ex;
-        } finally {
-            this.Desconecar();
+      if (n_est.next()) {
 
-        }
-        System.out.println("DAO ESTADO: " + estado);
-        return estado;
-    }
+        if (n_est.getInt(1) > 1) {
+          PreparedStatement st = this.getCon().prepareStatement("UPDATE  mesa SET id_estado=? WHERE id_mesa=?;");
+          st.setInt(1, 1);
+          st.setInt(2, mesa);
+          st.executeUpdate();
 
-    public String EstaPago(int mesa) throws Exception {
-        int pago;
-        String estado = null;
-        try {
-            this.Conectar();
+          PreparedStatement st2 = this.getCon().prepareStatement("INSERT INTO pedido "
+                  + "(id_mesa, hora, id_personal, cancelado)"
+                  + " VALUES (?, ?, ?, ?);");
 
-            PreparedStatement s2 = this.getCon().prepareStatement("select cancelado from control"
-                    + " where id_pedido in"
-                    + " (select max(id_pedido) from control group by id_mesa) and id_mesa=?;");
-            s2.setInt(1, mesa);
-            ResultSet n = s2.executeQuery();
-
-            if (n.next()) {
-                pago = n.getInt(1);
-                System.out.println("DAO: PAGO: " + pago);
-                if (pago == 1) {
-                    estado = "Cancelado";
-                } else {
-                    estado = "Pagado";
-                }
-            }
-
-        } catch (Exception ex) {
-            throw ex;
-        } finally {
-            this.Desconecar();
+          st2.setInt(1, mesa);
+          st2.setString(2, "00:00:00");
+          st2.setInt(3, 3);
+          st2.setInt(4, 0);
+          st2.executeUpdate();
 
         }
-        System.out.println("DAO ESTADO: " + estado);
-        return estado;
+      }
+
+    } catch (Exception ex) {
+      throw ex;
+    } finally {
+      this.Desconecar();
     }
 
-//    public EstadoPago FuncionEstadoPago(int mesa) throws Exception {
-//        
-//        EstadoPago EP=new EstadoPago();
-//    
-//       
-//        ResultSet rs;        
-//        int pago;
-//        boolean estado = false;
-//
-//        try {
-//
-//            this.Conectar();
-//
-//            PreparedStatement s2 = this.getCon().prepareStatement("select * from control \n"
-//                    + "where id_pedido in \n"
-//                    + "(select max(id_pedido) from control group by id_mesa) and id_mesa=?;");
-//            s2.setInt(1, mesa);
-//            ResultSet n = s2.executeQuery();
-//            if (n.next()) {
-//                pago = n.getInt(1);
-//
-//                if (pago == 1) {
-//                    EP.setDesHabilitarBoton(true);
-//                    EP.setEstado("Pagado");
-//                } else {
-//                    EP.setDesHabilitarBoton(false);
-//                    EP.setEstado("No Pagado");
-//                }
-//            }
-//
-//          
-//        
-//        } catch (Exception ex) {
-//            throw ex;
-//        } finally {
-//            this.Desconecar();
-//        }
-//
-//        return EP;
-//
-//}
+  }
 }
