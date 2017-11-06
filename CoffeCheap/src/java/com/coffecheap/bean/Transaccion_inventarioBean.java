bfab6fcb654ca87c3abfb14b283a0776f0ec1217
bryan <bryan@192.168.1.7>
2017-11-06 08:50:19 -0600

    Merge origin/master
    Conflicts:
    	CoffeCheap/src/java/com/coffecheap/dao/Dao.java

---
+++ CoffeCheap/src/java/com/coffecheap/bean/Transaccion_inventarioBean.java
@@ -0,0 +1,224 @@
+package com.coffecheap.bean;
+
+
+import com.coffecheap.dao.Transaccion_InventarioDAO;
+import com.coffecheap.modelo.Transaccion_inventario;
+import java.text.SimpleDateFormat;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class Transaccion_inventarioBean {
+
+    Transaccion_inventario traInv = new Transaccion_inventario();
+    List<Transaccion_inventario> lsttraInv = new ArrayList();
+    String fechaSus = null;
+ 
+    
+    public String getFechaSus() {
+        return fechaSus;
+    }
+
+    public void setFechaSus(String fechaSus) {
+        this.fechaSus = fechaSus;
+    }
+
+    public Transaccion_inventario getTraInv() {
+        return traInv;
+    }
+
+    public void setTraInv(Transaccion_inventario traInv) {
+        this.traInv = traInv;
+    }
+
+    public List<Transaccion_inventario> getLsttraInv() {
+        return lsttraInv;
+    }
+
+    public void setLsttraInv(List<Transaccion_inventario> lsttraInv) {
+        this.lsttraInv = lsttraInv;
+    }
+
+    public void registrar() throws Exception {
+
+        Transaccion_InventarioDAO dao = new Transaccion_InventarioDAO();
+
+        try {
+            
+            if (traInv.getTtransaccion().getId_tipo_transacciones() != 0) {
+                if (traInv.getProducto().getId_producto() != 0) {
+                    if (traInv.getId_transaccion() != 0) {
+                        
+                        SimpleDateFormat formateador = new SimpleDateFormat("yyyy-MM-dd");
+                        Date fecha = new Date();
+                        String fechaString = formateador.format(fecha);
+                        Date tranformada = formateador.parse(fechaString);
+
+                        fechaSus = fechaString;
+                        traInv.setFecha(tranformada);
+                        
+                        dao.registrar(traInv);
+                    }
+
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    
+             public void mostar() throws Exception
+    {
+        Transaccion_InventarioDAO dao;
+        
+        try
+        {
+            dao = new Transaccion_InventarioDAO();
+            lsttraInv =dao.mostrar();
+        }catch(Exception e)
+        {
+            throw e;
+        }
+    }
+       
+            public void mostarPorParametro() throws Exception
+    {
+        Transaccion_InventarioDAO dao;
+        
+        try
+        {
+            dao = new Transaccion_InventarioDAO();
+            lsttraInv =dao.mostrarPorPrarametro(traInv);
+        }catch(Exception e)
+        {
+            throw e;
+        }
+    }
+                     public void mostarPorParametroEspecial() throws Exception
+    {
+        Transaccion_InventarioDAO dao;
+        
+        try
+        {
+            
+              
+            dao = new Transaccion_InventarioDAO();
+            lsttraInv =dao.mostrarPorPrarametroEspecial(traInv);
+        }catch(Exception e)
+        {
+            throw e;
+        }
+    }
+                     
+         public void mostarPorRangoEspecial() throws Exception
+    {
+        Transaccion_InventarioDAO dao;
+        
+        try
+        {
+            
+              
+            dao = new Transaccion_InventarioDAO();
+            lsttraInv =dao.mostrarPorRangoEspecial(traInv);
+        }catch(Exception e)
+        {
+            throw e;
+        }
+    }
+         
+          public void leerParaModificarProducto() throws Exception
+    {
+        Transaccion_InventarioDAO dao;
+        
+        try
+        {
+            dao = new Transaccion_InventarioDAO();
+            lsttraInv =dao.listarParaModificarProducto();
+        }catch(Exception e)
+        {
+            throw e;
+        }
+    }
+               public void leerParaModificarTipoTransacciones() throws Exception
+    {
+        Transaccion_InventarioDAO dao;
+        
+        try
+        {
+            dao = new Transaccion_InventarioDAO();
+            lsttraInv =dao.listarParaModificarTtransaccion();
+        }catch(Exception e)
+        {
+            throw e;
+        }
+    }
+               public void buscar()throws Exception
+               {
+                   Transaccion_InventarioDAO dao;
+                   Transaccion_inventario temp;
+                   
+                   try
+                   {
+                       dao = new Transaccion_InventarioDAO();
+                       temp = dao.leerParaModificar(traInv);
+                       if(traInv!=null)
+                       {
+                           this.traInv= temp;
+                       }
+                       
+                      
+                   }catch(Exception e)
+                   {
+                       throw e;
+                   }
+               }
+    /*         
+                 public void eliminar() throws Exception
+    {
+        
+        Transaccion_InventarioDAO dao = new Transaccion_InventarioDAO();
+        
+        try
+        {
+            dao.eliminar(oc);
+            
+        }catch(Exception e)
+        {
+            throw e;
+        }
+    }
+                 public void listaParaModificar() throws Exception
+    {
+        Transaccion_InventarioDAO dao = new Transaccion_InventarioDAO();
+        
+        try
+        {
+            lstoc =dao.listarParaModificar();
+        }catch(Exception e)
+        {
+            throw e;
+        }
+    }
+                 public void modificar() throws Exception
+    {
+        
+        Transaccion_InventarioDAO dao = new Transaccion_InventarioDAO();
+        
+        try
+        {
+            dao.modificar(oc);
+            
+        }catch(Exception e)
+        {
+            throw e;
+        }
+    }
+     */
+}
---
+++ CoffeCheap/src/java/com/coffecheap/dao/Transaccion_InventarioDAO.java
@@ -0,0 +1,324 @@
+package com.coffecheap.dao;
+
+import com.coffecheap.modelo.Transaccion_inventario;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Transaccion_InventarioDAO extends Dao
+{
+    public void registrar(Transaccion_inventario traInv) throws Exception
+    {
+        try
+        {   
+        SimpleDateFormat formateador = new SimpleDateFormat("yyyy-MM-dd");
+        
+            this.Conectar();
+                
+            PreparedStatement st = this.getCon().prepareStatement("insert into transaccion_inventario values(?,?,?,?)");
+            st.setInt(1, traInv.getId_transaccion());
+            st.setString(2, formateador.format(traInv.getFecha()));
+            st.setInt(3, traInv.getProducto().getId_producto());
+            st.setInt(4, traInv.getTtransaccion().getId_tipo_transacciones());
+            st.executeUpdate();   
+        }catch(Exception e)
+        {
+            throw e;
+        }finally
+        {
+            this.Desconecar();
+        }
+    }
+      public List<Transaccion_inventario> mostrar() throws Exception
+    {
+        List<Transaccion_inventario> lista;
+        ResultSet rs;
+        
+        try
+        {
+            this.Conectar();
+            PreparedStatement st = getCon().prepareCall("select traInv.id_transaccion, traInv.fecha, "
+                    + "pro.nombre_producto, tipTran.nombre_trasaccion from transaccion_inventario as "
+                    + "traInv inner join producto as pro on(traInv.id_producto = pro.id_producto) "
+                    + "inner join tipo_transacciones as tipTran on(traInv.id_tipo_transaccion = "
+                    + "tipTran.id_tipo_transacciones)");
+            rs=st.executeQuery();
+            lista = new ArrayList();
+            
+            while(rs.next())
+            {
+                Transaccion_inventario tra = new Transaccion_inventario();
+                tra.setId_transaccion(rs.getInt(1));
+                tra.setFecha((rs.getDate(2)));
+                tra.getProducto().setNombre(rs.getString(3));
+                tra.getTtransaccion().setNombre(rs.getString(4));
+                lista.add(tra);
+            }
+        }catch(Exception e)
+        {
+            throw e;
+        }finally
+        {
+            this.Desconecar();
+        }
+        return lista;
+    }   
+    
+    
+      public List<Transaccion_inventario> mostrarPorPrarametro(Transaccion_inventario  traInv) throws Exception
+    {
+        List<Transaccion_inventario> lista;
+        ResultSet rs;
+        
+        try
+        {
+            this.Conectar();
+            PreparedStatement st = getCon().prepareCall("select traInv.id_transaccion, traInv.fecha, pro.nombre_producto, tipTran.nombre_trasaccion "
+                    + "from transaccion_inventario as traInv inner join producto as pro on(traInv.id_producto = pro.id_producto) inner join tipo_transacciones as "
+                    + "tipTran on(traInv.id_tipo_transaccion = tipTran.id_tipo_transacciones) where traInv.id_transaccion=?");
+            st.setInt(1, traInv.getId_transaccion());
+            rs=st.executeQuery();
+            lista = new ArrayList();
+            
+            while(rs.next())
+            {
+                   Transaccion_inventario tra = new Transaccion_inventario();
+                tra.setId_transaccion(rs.getInt(1));
+                tra.setFecha((rs.getDate(2)));
+                tra.getProducto().setNombre(rs.getString(3));
+                tra.getTtransaccion().setNombre(rs.getString(4));
+                lista.add(tra);
+               
+            }
+        }catch(Exception e)
+        {
+            throw e;
+        }finally
+        {
+            this.Desconecar();
+        }
+        return lista;
+    } 
+      
+      public List<Transaccion_inventario> mostrarPorPrarametroEspecial(Transaccion_inventario  traInv) throws Exception
+    {
+        List<Transaccion_inventario> lista;
+        ResultSet rs;
+        try
+        {
+            this.Conectar();
+            
+                  SimpleDateFormat formateador = new SimpleDateFormat("yyyy-MM-dd");
+                    
+            PreparedStatement st = getCon().prepareCall("select traInv.id_transaccion, traInv.fecha, pro.nombre_producto, "
+                    + "tipTran.nombre_trasaccion from transaccion_inventario as traInv inner join producto as pro on(traInv.id_producto = "
+                    + "pro.id_producto) inner join tipo_transacciones as tipTran on(traInv.id_tipo_transaccion = tipTran.id_tipo_transacciones) "
+                    + "where traInv.fecha=?");
+            
+            //String nu=formateador.format(traInv.getFecha());
+           
+            st.setString(1, traInv.getProducto().getNombre());
+            rs=st.executeQuery();
+            lista = new ArrayList();
+            
+            while(rs.next())
+            {
+                   Transaccion_inventario tra = new Transaccion_inventario();
+                tra.setId_transaccion(rs.getInt(1));
+                tra.setFecha((rs.getDate(2)));
+                tra.getProducto().setNombre(rs.getString(3));
+                tra.getTtransaccion().setNombre(rs.getString(4));
+                lista.add(tra);
+               
+            }
+        }catch(Exception e)
+        {
+            throw e;
+        }finally
+        {
+            this.Desconecar();
+        }
+        return lista;
+    } 
+      
+      public List<Transaccion_inventario> mostrarPorRangoEspecial(Transaccion_inventario  traInv) throws Exception
+    {
+        List<Transaccion_inventario> lista;
+        ResultSet rs;
+        try
+        {
+            this.Conectar();
+            
+                  SimpleDateFormat formateador = new SimpleDateFormat("yyyy-MM-dd");
+                    
+            PreparedStatement st = getCon().prepareCall("select traInv.id_transaccion, traInv.fecha, pro.nombre_producto,tipTran.nombre_trasaccion "
+                    + "from transaccion_inventario as traInv inner join producto as pro on(traInv.id_producto = pro.id_producto) inner join tipo_transacciones "
+                    + "as tipTran on(traInv.id_tipo_transaccion = tipTran.id_tipo_transacciones) where traInv.fecha BETWEEN ? AND ?");
+            
+            //String nu=formateador.format(traInv.getFecha());
+           
+            st.setString(1,traInv.getFechaUno());
+            st.setString(2, traInv.getFechaDos());
+            rs=st.executeQuery();
+            lista = new ArrayList();
+            
+            while(rs.next())
+            {
+                   Transaccion_inventario tra = new Transaccion_inventario();
+                tra.setId_transaccion(rs.getInt(1));
+                tra.setFecha((rs.getDate(2)));
+                tra.getProducto().setNombre(rs.getString(3));
+                tra.getTtransaccion().setNombre(rs.getString(4));
+                lista.add(tra);
+               
+            }
+        }catch(Exception e)
+        {
+            throw e;
+        }finally
+        {
+            this.Desconecar();
+        }
+        return lista;
+    }   
+      
+    
+      public List<Transaccion_inventario> listarParaModificarProducto() throws Exception
+    {
+        List<Transaccion_inventario> lista;
+        ResultSet rs;
+        try
+        {
+            this.Conectar();
+            PreparedStatement st = getCon().prepareCall("select id_producto, nombre_producto from producto");
+            rs=st.executeQuery();
+            lista = new ArrayList();
+            
+            while(rs.next())
+            {
+                Transaccion_inventario traInv = new Transaccion_inventario();
+                traInv.getProducto().setId_producto(rs.getInt(1));
+                traInv.getProducto().setNombre(rs.getString(2));
+                lista.add(traInv);
+            }
+        }catch(Exception e)
+        {
+            throw e;
+        }finally
+        {
+            this.Desconecar();
+        }
+        return lista;
+    }
+      
+        public List<Transaccion_inventario> listarParaModificarTtransaccion() throws Exception
+    {
+        List<Transaccion_inventario> lista;
+        ResultSet rs;
+        try
+        {
+            this.Conectar();
+            PreparedStatement st = getCon().prepareCall("select id_tipo_transacciones, nombre_trasaccion from tipo_transacciones");
+            rs=st.executeQuery();
+            lista = new ArrayList();
+            
+            while(rs.next())
+            {
+                Transaccion_inventario traInv = new Transaccion_inventario();
+                traInv.getTtransaccion().setId_tipo_transacciones(rs.getInt(1));
+                traInv.getTtransaccion().setNombre(rs.getString(2));
+                lista.add(traInv);
+            }
+        }catch(Exception e)
+        {
+            throw e;
+        }finally
+        {
+            this.Desconecar();
+        }
+        return lista;
+    }
+      
+      public Transaccion_inventario leerParaModificar(Transaccion_inventario  traInv) throws Exception
+    {
+        Transaccion_inventario traInvAden = null;
+        ResultSet rs;
+        
+        try
+        {
+            this.Conectar();
+            PreparedStatement st = getCon().prepareCall("select id_transaccion, fecha, id_producto, id_tipo_transaccion from "
+                    + "transaccion_inventario where id_transaccion=?");
+            st.setInt(1, traInv.getId_transaccion());
+            rs=st.executeQuery();
+            
+            
+            while(rs.next())
+            {
+                   traInvAden = new Transaccion_inventario();
+                   
+                traInvAden.setId_transaccion(rs.getInt(1));
+                traInvAden.setFecha((rs.getDate(2)));
+                traInvAden.getProducto().setNombre(rs.getString(3));
+                traInvAden.getTtransaccion().setNombre(rs.getString(4));
+                
+               
+            }
+        }catch(Exception e)
+        {
+            throw e;
+        }finally
+        {
+            this.Desconecar();
+        }
+        return traInvAden;
+    } 
+    /*  
+         public void eliminar(orden_compras orco) throws Exception
+    {
+        try
+        {
+         this.conectar();
+            PreparedStatement st = this.getCn().prepareStatement("delete from orden_compras where id_orden_compras=?");
+            st.setInt(1, orco.getId_orden_compras());
+            st.executeUpdate();
+        }catch(Exception e)
+        {
+            throw e;
+        }finally
+        {
+            this.cerrar();
+        }
+    }
+*/         
+              public void modificar(Transaccion_inventario orco) throws Exception
+    {
+        try
+        {
+         this.Conectar();
+            PreparedStatement st = this.getCon().prepareStatement("update orden_compras set  fecha_orden=?, "
+                    + "fecha_entrega=?, cantidad_orden=?, id_producto=?, id_proveedor=?, precio=? "
+                    + "where id_orden_compras=?");
+            /*st.setString(1, orco.getFecha_orden());
+            st.setString(2, orco.getFecha_entrega());
+            st.setInt(3, orco.getCantidad_orden());
+            st.setInt(4, orco.getId_producto());
+            st.setInt(5, orco.getId_proveedor());
+            st.setInt(6, orco.getPrecio());
+            st.setInt(7, orco.getId_orden_compras());
+*/
+            st.executeUpdate();
+        }catch(Exception e)
+        {
+            throw e;
+        }finally
+        {
+            this.Desconecar();
+        }
+    }
+
+}
