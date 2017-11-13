bd1ce4da2d4580ca6f963954761f2282b0e2ff51
bryan <bryan@192.168.1.7>
2017-11-13 11:46:01 -0600

    Merge origin/master
    Conflicts:
    	CoffeCheap/src/java/com/coffecheap/dao/Dao.java

--- CoffeCheap/src/java/com/coffecheap/dao/Dao.java
+++ CoffeCheap/src/java/com/coffecheap/dao/Dao.java
@@ -19,8 +19,8 @@ public class Dao {
     public void Conectar() throws Exception {
         try {
             Class.forName("com.mysql.jdbc.Driver");            
-           //con = DriverManager.getConnection("jdbc:mysql://localhost/coffechip?user=root&password=intecap");                       
-            con = DriverManager.getConnection("jdbc:mysql://localhost/coffechip?user=root");
+           con = DriverManager.getConnection("jdbc:mysql://localhost/coffechip?user=root&password=intecap");                       
+            //con = DriverManager.getConnection("jdbc:mysql://localhost/coffechip?user=root");
             //con = DriverManager.getConnection("jdbc:mysql://192.168.22.52/coffechip?user=lezana&password=lezana");
         } catch (Exception e) {
             throw e;
